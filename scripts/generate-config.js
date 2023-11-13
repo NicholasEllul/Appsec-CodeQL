df5b855af33df33a78197ae990fd485e6ffe7b10
witmicko <witmicko@users.noreply.github.com>
2023-11-13 14:40:38 +0000

    debug

--- scripts/generate-config.js
+++ scripts/generate-config.js
@@ -7,6 +7,26 @@ const template = fs.readFileSync('config/codeql-template.yml', 'utf8');
 
 
 
+function printDirectoryTree(dir, indent = '') {
+  const files = fs.readdirSync(dir);
+
+  files.forEach((file, index) => {
+    const filePath = path.join(dir, file);
+    const isDirectory = fs.statSync(filePath).isDirectory();
+
+    // Print the current file or directory
+    console.log(`${indent}${files.length - 1 === index ? '└─' : '├─'} ${file}`);
+
+    // If it's a directory, recursively print its contents
+    if (isDirectory) {
+      printDirectoryTree(filePath, `${indent}${files.length - 1 === index ? '  ' : '│ '}`);
+    }
+  });
+}
+
+
+
+
 console.log('>>>>>', process.env.INPUT_PATHS_IGNORED);
 console.log('>>>>>', process.env.INPUT_RULES_IGNORED);
 const output = ejs.render(template, {
@@ -15,3 +35,4 @@ const output = ejs.render(template, {
 });
 console.log(output);
 fs.writeFileSync('.github/codeql-config.yml', output);
+printDirectoryTree('./');
