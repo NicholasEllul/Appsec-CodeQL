9a01469e78e70a94aa51abbddafad25a78a1b883
witmicko <witmicko@users.noreply.github.com>
2023-11-13 14:47:43 +0000

    asd

--- scripts/generate-config.js
+++ scripts/generate-config.js
@@ -1,6 +1,8 @@
 // scripts/generate-config.js
 
 const fs = require('fs');
+const path = require('path');
+
 const ejs = require('ejs');
 
 const template = fs.readFileSync('config/codeql-template.yml', 'utf8');
