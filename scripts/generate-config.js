32a1b240ed4323597e6ad6cade7f988864af25ab
witmicko <witmicko@users.noreply.github.com>
2023-11-15 12:34:47 +0000

    asd

--- scripts/generate-config.js
+++ scripts/generate-config.js
@@ -7,9 +7,22 @@ const ejs = require('ejs');
 
 const template = fs.readFileSync('config/codeql-template.yml', 'utf8');
 
-const output = ejs.render(template, {
+const inputs = {
   pathsIgnored: process.env.INPUT_PATHS_IGNORED.split(','),
-  queries: JSON.parse(process.env.QUERIES)
+  queries: JSON.parse(process.env.QUERIES),
+  repo: process.env.REPO,
+};
+console.log(`>>>>>`, inputs.repo);
+// const loadConfig = (repo) => {
+//   return fs.readFileSync('./qls-packs' + repo + '.qls');
+// };
+
+// combine queries and repo config
+// const q = {...JSON.parse(process.env.QUERIES) || {}, ...loadConfig(reponame)}
+
+const output = ejs.render(template, {
+  pathsIgnored,
+  queries
 });
 console.log(output);
 fs.writeFileSync('.github/codeql-config.yml', output);
